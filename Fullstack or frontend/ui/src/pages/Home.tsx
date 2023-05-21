9d8f4bd1fdde560dfcfbda4a754957cfbf5b695f
Guillermo Sotelo <guillermo.sotelo@conexiogroup.com>
2023-05-21 18:49:25 +0200

    updated d&d

--- Fullstack or frontend/ui/src/pages/Home.tsx
+++ Fullstack or frontend/ui/src/pages/Home.tsx
@@ -7,21 +7,23 @@ const Home: React.FC = (props: Props) => {
   const [text, setText] = useState<string>('')
   const [filename, setFilename] = useState<string>('')
   const [generatedText, setGeneratedText] = useState<string>('')
+  const [dragging, setDragging] = useState<boolean>(false)
 
   const loadFile = (e: SyntheticEvent) => {
     const { files } = e.target as HTMLInputElement
+    if (files) readFile(files)
+  }
+
+  const readFile = (files: FileList) => {
     if (files && files.length > 0) {
       const reader = new FileReader()
-
       reader.onload = (event) => {
         const fileContent = event?.target?.result
         setText(String(fileContent) || '')
       }
-
       reader.readAsText(files[0])
       setFilename(files[0].name ? files[0].name.split('.')[0] : '')
     }
-
   }
 
   const convertText = async () => {
@@ -36,12 +38,34 @@ const Home: React.FC = (props: Props) => {
     if (input) input.click()
   }
 
+  const handleDrop = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault()
+    setDragging(false)
+    const files = e.dataTransfer.files
+    readFile(files)
+  }
+
   return (
     <div className='home__container'>
       <div className="home__converter">
         <h2 className="home__title">HiQ Text Converter</h2>
         {!text ?
-          <h4 className="home__converter-label" onClick={openFileLoader}>Choose file or drag it here</h4>
+          <h4
+            className="home__converter-label"
+            onDragOver={e => e.preventDefault()}
+            onDragEnter={e => {
+              e.preventDefault()
+              setDragging(true)
+            }}
+            onDragLeave={e => {
+              e.preventDefault()
+              setDragging(false)
+            }}
+            onDrop={handleDrop}
+            style={{ backgroundColor: dragging ? '#babae0' : '' }}
+            onClick={openFileLoader}>
+            Choose file or drag it here
+          </h4>
           : ''}
         <input type='file' id='file-loader' onChange={loadFile} style={{ height: 0 }} accept='.rtf,.txt,.md,.file' />
         {text ?
